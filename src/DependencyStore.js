018665049794a32f7e081e4a7551678b89ed2f22
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-12-02 17:17:31 -0500

    Finalized DependencyStore POC
    TODO: Support local scripts not in the public directory

--- src/DependencyStore.js
+++ src/DependencyStore.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import {Helmet} from "react-helmet";
 import HookStore from './Hooks';
 import ErrorBoundary from './components/ErrorBoundary';
